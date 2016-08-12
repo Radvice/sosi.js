48aabff2551a123330b194d0c161121a75b1a29c
adambar <adam.bar@brightinventions.pl>
2016-08-12 09:40:51 +0200

    Add EPSG:32633 projection

--- dist/SOSI_lite.js
+++ dist/SOSI_lite.js
@@ -1,6 +1,9 @@
 if (!(typeof require=="undefined")) { /* we are running inside node.js */
     var _ = require("underscore");
     var proj4 = require("proj4");
+
+	proj4.defs('EPSG:32633', '+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs');
+
     var window = window || {};
     window.SOSI = window.SOSI || {};
 }
